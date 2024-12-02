4a35e1f493ab159838dd8d63b589f149517f2718
Manish <mandalmanishjjjp@.com>
2024-12-02 11:21:31 +0530

    add models file of backend

--- src/index.js
+++ src/index.js
@@ -15,6 +15,16 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(` server is running at port : ${process.env.PORT}`);
+    
+  })
+})
+.catch((error)=>{
+  console.log("MONGODB connection is failed !!!! ",error);
+  
+})
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,12 @@
+class ApiResponse {
+    constructor(
+       
+     stausCode,data,
+     message="success"   
+    ){
+        this.stausCode = stausCode
+        this.data = data
+        this.message = message
+        this.success = stausCode < 400 
+    }
+}
